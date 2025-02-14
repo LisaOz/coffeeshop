93d9022159a4ce8bfea4144ffb85736f76784556
Lisa <frolovaie9@gmail.com>
2025-02-14 16:10:34 +0000

    Created a form to enter orde details. Created view to handle the form and create a new order

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Order, OrderItem
+
 
 # Register your models here.
+
+"""
+OrderInLine class is created to include the OrderItem model on the same edit page with its model
+"""
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ["product"]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "address",
+        "postal_code",
+        "city",
+        "paid",
+         "created",
+        "updated"
+    ]
+
+    list_filter = ["paid", "created", "updated"]
+    inlines = [OrderItemInline]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,34 @@
+from cart.cart import Cart
 from django.shortcuts import render
+from .forms import OrderCreateForm
+from .models import OrderItem
 
-# Create your views here.
+
+"""
+View for the 'order_create' 
+"""
+
+
+def order_create(request):
+    cart = Cart(request)  # get the current cart from the session cart
+    if request.method == 'POST': # Validates the data sent in the request
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:  # iterate over all items in the cart and create a new order in the database
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+
+
+            cart.clear() # Clear the cart
+
+
+    return render(
+        request,
+        'orders/order/create.html',
+        {'cart': cart, 'form': form}
+    )
