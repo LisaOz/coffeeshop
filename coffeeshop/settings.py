bb86fefb1dc3bacc3826ff7d05298a97c0be5ea9
Lisa <frolovaie9@gmail.com>
2025-02-16 16:33:01 +0000

    Created new application 'payments' and added it to the settings.py to activate

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payments.apps.PaymentConfig',
     'shop.apps.ShopConfig',
 ]
 
