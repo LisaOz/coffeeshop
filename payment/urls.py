ccd02406cf647886e9a9f677f6593f2504a5174e
Lisa <frolovaie9@gmail.com>
2025-02-17 13:20:43 +0000

    Created URLs for the payments workflow: process, completed, and cancelled

---
+++ payment/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+app_name = "payment"
+
+"""
+URLs for the payments workflow
+"""
+urlpatterns = [
+    # view to display the order summary, create Stripe checkout session, and redirect to the Stripe-hosted payment form
+    path('process/', views.payment_process, name='process'),
+
+    # view for Stripe to redirect the user if the payment is successful
+    path('completed/', views.payment_completed, name='completed'),
+
+    # view for Stripe to redirect the user if the payment is cancelled
+    path('canceled/', views.payment_canceled, name='canceled'),
+
+]
