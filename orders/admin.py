26fcc6d1b602eede124dd13a5ea544fb21fc8365
Lisa <frolovaie9@gmail.com>
2025-02-17 20:40:42 +0000

    Implemented link to each order on the list on the administration site

--- orders/admin.py
+++ orders/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
 
 
 # Register your models here.
 
+
+# Method to add a link to each Order object on the list page of the admin site.
+# It takes an Order object as an argument and returns an HTML link with the payment URL in Stripe
+
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)  # here use 'mark-safe avoid auto-escape. Don't use on the inputs coming from the users!!
+    return ""
+
+
+order_payment.short_description = "Stripe payment"
+
 """
 OrderInLine class is created to include the OrderItem model on the same edit page with its model
 """
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
@@ -23,7 +40,8 @@ class OrderAdmin(admin.ModelAdmin):
         "postal_code",
         "city",
         "paid",
-         "created",
+        order_payment,
+        "created",
         "updated"
     ]
 
