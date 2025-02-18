b3eb3d1a85b2dbaeb1ab4f8dfb6faa1808c2b9dc
Lisa <frolovaie9@gmail.com>
2025-02-18 11:19:57 +0000

    Added a function to return HTML link to the admin_order_detaul URL to the object argument in the function

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import reverse
 from .models import Order, OrderItem
 from django.utils.safestring import mark_safe
 import csv
@@ -70,6 +71,16 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = 'Export to CSV'
 
 
+"""
+This is a method that takes an Order object as an argument and returns an HTTP link from the admin order_detail URL.
+to avoid auto-escaping of HTML by Django, use mark_safe function
+"""
+
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -84,7 +95,8 @@ class OrderAdmin(admin.ModelAdmin):
         "paid",
         order_payment,
         "created",
-        "updated"
+        "updated",
+        order_detail,
     ]
 
     list_filter = ["paid", "created", "updated"]
