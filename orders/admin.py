0bd21e690238aefaf4cdb6e17968be31dd71e7c9
Lisa <frolovaie9@gmail.com>
2025-02-18 10:09:06 +0000

    Implemented a generic administration action that can be added to any ModelAdmin class

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Order, OrderItem
 from django.utils.safestring import mark_safe
+import csv
+import datetime
+from django.http import HttpResponse
 
 
 # Register your models here.
@@ -47,3 +50,41 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+
+
+"""
+Method to export the data into the CSV file
+"""
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = (
+        f'attachment; filename={opts.verbose_name}.csv'
+    )
+
+    response = HttpResponse(content_type='text/csv')  # tell the browser to treat the response as a CSV file
+    response['Content-Disposition'] = content_disposition  # header to indicate  HTTP response contains an attached file
+    writer = csv.writer(response)  # create a CSV writer to write to the response object
+    fields = [
+        field
+        for field in opts.get_fields()  # get the model fields dynamically
+        if not field.many_to_many and not field.one_to_many  # exclude many-to-many and one-to-many relationships fields
+    ]
+    # First row with the header info
+    writer.writerow([field.verbose_name for field in fields])  # write the header row including the field names
+
+    # Write data rows, iterating over the QuerySer and writing a row for each returned object
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')  # output value for the csv has to be a string
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+# Display name for the action in the action's drop=down element of the admin site
+export_to_csv.short_description = 'Export to CSV'
