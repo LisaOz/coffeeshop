1d91a75d1f12f7f3a6ed9cea82fd6fd712625142
Lisa <frolovaie9@gmail.com>
2025-05-23 14:05:25 +0100

    Debugged the code to generate the PDF file for invoice using Weasyprint correctly

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,7 @@
+import tempfile
 from functools import wraps
 
+import weasyprint
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
@@ -10,7 +12,7 @@ from .forms import OrderCreateForm
 from .models import Order, OrderItem
 from shop.models import StaffRole
 from .tasks import order_created
-import weasyprint
+from weasyprint import HTML, CSS
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
@@ -22,25 +24,24 @@ Decorate is used to indicate tht only staff users can access this view.
 
 @staff_member_required
 def admin_order_pdf(request, order_id):
-    order = get_object_or_404(Order, id=order_id)  # get the order object with the given ID
+    order = get_object_or_404(Order, id=order_id)
 
-    # use Django's render to string function to render a response, rendered HTML response is saved into a variable
+    # Render the HTML template to a string
     html = render_to_string('orders/order/pdf.html', {'order': order})
 
-    # Generate a new HttpResponse obj specifying the content type and including
-    # the Content-Disposition header to specify the file name
+    # Create an HTTP response object with PDF content type
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
 
-    # use WeasyPrint to generate a PDF file from the rendered HTML code and write to the HTTP response object
-    weasyprint.HTML(string=html).write_pdf(  # Generate the PDF
-        response,
-        stylesheets=[weasyprint.CSS(finders.find('css/pdf.css'))]
-    )
-
-    # to locate the file you use the finder function of the staticfile module. Return generated response
-    return response
-
+    try:
+        # Write the PDF to the HTTP response
+        weasyprint.HTML(string=html).write_pdf(
+            response,
+            stylesheets=[weasyprint.CSS(finders.find('css/pdf.css'))]
+        )
+        return response
+    except Exception as e:
+        return HttpResponse("Error generating PDF", status=500)
 
 
 """
